8e4f6cbb7950af3576b664ec441195821f029328
Bruno <bgianini@gmail.com>
2017-07-20 22:04:35 -0300

    Inicio

---
+++ ColarGridExcel/Form1.Designer.vb
@@ -0,0 +1,132 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Column1 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column2 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column3 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column4 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Column5 = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.btnImportarExcel = New System.Windows.Forms.Button()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.lblProcessando = New System.Windows.Forms.Label()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Column1, Me.Column2, Me.Column3, Me.Column4, Me.Column5})
+        Me.DataGridView1.Location = New System.Drawing.Point(12, 33)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(871, 381)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'Column1
+        '
+        Me.Column1.HeaderText = "Column1"
+        Me.Column1.Name = "Column1"
+        '
+        'Column2
+        '
+        Me.Column2.HeaderText = "Column2"
+        Me.Column2.Name = "Column2"
+        '
+        'Column3
+        '
+        Me.Column3.HeaderText = "Column3"
+        Me.Column3.Name = "Column3"
+        '
+        'Column4
+        '
+        Me.Column4.HeaderText = "Column4"
+        Me.Column4.Name = "Column4"
+        '
+        'Column5
+        '
+        Me.Column5.HeaderText = "Column5"
+        Me.Column5.Name = "Column5"
+        '
+        'btnImportarExcel
+        '
+        Me.btnImportarExcel.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnImportarExcel.Location = New System.Drawing.Point(766, 453)
+        Me.btnImportarExcel.Name = "btnImportarExcel"
+        Me.btnImportarExcel.Size = New System.Drawing.Size(117, 23)
+        Me.btnImportarExcel.TabIndex = 1
+        Me.btnImportarExcel.Text = "Importar Excel"
+        Me.btnImportarExcel.UseVisualStyleBackColor = True
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 424)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(871, 23)
+        Me.ProgressBar1.TabIndex = 2
+        '
+        'lblProcessando
+        '
+        Me.lblProcessando.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.lblProcessando.AutoSize = True
+        Me.lblProcessando.Font = New System.Drawing.Font("Trebuchet MS", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblProcessando.ForeColor = System.Drawing.Color.Red
+        Me.lblProcessando.Location = New System.Drawing.Point(226, 221)
+        Me.lblProcessando.Name = "lblProcessando"
+        Me.lblProcessando.Size = New System.Drawing.Size(442, 46)
+        Me.lblProcessando.TabIndex = 3
+        Me.lblProcessando.Text = "Processando... Aguarde!"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(895, 488)
+        Me.Controls.Add(Me.lblProcessando)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.btnImportarExcel)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnImportarExcel As Button
+    Friend WithEvents Column1 As DataGridViewTextBoxColumn
+    Friend WithEvents Column2 As DataGridViewTextBoxColumn
+    Friend WithEvents Column3 As DataGridViewTextBoxColumn
+    Friend WithEvents Column4 As DataGridViewTextBoxColumn
+    Friend WithEvents Column5 As DataGridViewTextBoxColumn
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents lblProcessando As Label
+End Class
---
+++ ColarGridExcel/Form1.vb
@@ -0,0 +1,96 @@
+﻿Imports System.Threading
+
+Public Class Form1
+
+    Dim t As Thread
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        lblProcessando.Visible = False
+        ProgressBar1.Visible = False
+    End Sub
+
+    Private Sub btnImportarExcel_Click(sender As Object, e As EventArgs) Handles btnImportarExcel.Click
+
+        'ColarDoExcel()
+        Dim vAreaTransferencia As String() = Clipboard.GetText.Split(vbNewLine)
+
+
+        Control.CheckForIllegalCrossThreadCalls = False
+        t = New Thread(Sub() ColarDoExcel(vAreaTransferencia))
+        t.Start()
+
+    End Sub
+
+
+
+    ' No evento KeyDown da DataGridView
+    Private Sub DataGridView1_KeyDown(ByVal sender As System.Object, ByVal e As System.Windows.Forms.KeyEventArgs) Handles DataGridView1.KeyDown
+
+        ' Caso as teclas pressinadas sejam CTRL+V
+        If e.Control AndAlso e.KeyCode = Keys.V Then
+            'ColarDoExcel(x)
+
+        End If
+
+    End Sub
+
+    Private Sub ColarDoExcel(ByVal pAreaTransferencia() As String)
+        Try
+            'DataGridView1.ScrollBars = ScrollBars.None
+            DataGridView1.Visible = False
+
+            lblProcessando.Visible = True
+            ProgressBar1.Visible = True
+
+
+            ProgressBar1.Maximum = pAreaTransferencia.Length - 1
+
+            ' Ciclo nas linhas copiadas
+            'For Each line As String In Clipboard.GetText.Split(vbNewLine)
+            For Each line As String In pAreaTransferencia
+
+                If line.Length = 1 Then Exit For 'Para retirar a ultima linha (estava com bug)
+
+                ProgressBar1.Value += 1
+                ' Separa as colunas referentes à linha actual
+                Dim item() As String = line.Trim.Split(vbTab)
+
+                ' Se o número de colunas for diferente mostra uma mensagem de erro
+                If item.Length <> Me.DataGridView1.ColumnCount Then
+                    Dim str As String = "O número de colunas copiadas é diferente" &
+                                                " do número de colunas da Grid"
+                    Throw New Exception(str)
+                End If
+
+                ' Adicionar a linha a DataGridView
+                Me.DataGridView1.Rows.Add(item)
+
+
+            Next
+
+            MessageBox.Show("Informações do Excel coladas com sucesso!", My.Application.Info.Title, MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        Catch ex As Exception
+            ' Mensagem de erro caso exista
+            MessageBox.Show(ex.Message, My.Application.Info.Title, MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+            lblProcessando.Visible = False
+            ProgressBar1.Visible = False
+
+            DataGridView1.Visible = True
+            DataGridView1.ScrollBars = ScrollBars.Vertical
+        End Try
+
+    End Sub
+
+    Private Sub AvisoNaTela(ByVal pTexto As String)
+        Dim lbl As New Label
+        lbl.Size = New System.Drawing.Size(159, 23) 'set your size (if required)
+        lbl.Location = New System.Drawing.Point(12, 180) 'set your location
+        lbl.Text = pTexto 'set the text for your label
+        Me.Controls.Add(lbl)  'add your new control to your forms control collection
+    End Sub
+
+
+End Class
